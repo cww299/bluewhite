66c3ee7b1acfa5ce4b22b9b0881863dcc9ecf313
Zjiezi <597929460@qq.com>
2018-05-24 14:25:55 +0800

    *

--- src/main/java/com/bluewhite/production/task/action/TaskAction.java
+++ src/main/java/com/bluewhite/production/task/action/TaskAction.java
@@ -32,7 +32,7 @@ private static final Log log = Log.getLog(TaskAction.class);
 	{
 		clearCascadeJSON = ClearCascadeJSON
 				.get()
-				.addRetainTerm(Task.class,"id","bacth","productName","userIds","procedureName","number","status","expectTime"
+				.addRetainTerm(Task.class,"id","userNames","bacth","productName","userIds","procedureName","number","status","expectTime"
 						,"taskTime","BPrice","type")
 				.addRetainTerm(Bacth.class,"id","bacthNumber");
 	}
