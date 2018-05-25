2645fb683f0ec6e6b114151310ed897a8ac0bb2f
Zjiezi <597929460@qq.com>
2018-05-25 16:40:35 +0800

    *

--- src/main/java/com/bluewhite/production/farragotask/entity/FarragoTask.java
+++ src/main/java/com/bluewhite/production/farragotask/entity/FarragoTask.java
@@ -4,6 +4,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import com.bluewhite.base.BaseEntity;
+
 /**
  * 
  * 杂工（混杂工作，一些除正常之外的工作）
@@ -12,7 +14,7 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "pro_farrago_task")
-public class FarragoTask {
+public class FarragoTask  extends BaseEntity<Long>{
 	
 	/**
 	 * 杂工所属部门类型 (1=一楼质检，2=一楼包装，3=二楼针工)
@@ -26,6 +28,18 @@ public class FarragoTask {
 	 */
 	@Column(name = "name")
 	private String name;
+	
+	/**
+	 * 杂工实际成本费用
+	 */
+	@Column(name = "price")
+	private Double price;
+	
+	/**
+	 * 杂工实际工作时间
+	 */
+	@Column(name = "time")
+	private Double time;
 
 
 	public Integer getType() {
@@ -46,6 +60,27 @@ public class FarragoTask {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+
+	public Double getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+
+	public Double getTime() {
+		return time;
+	}
+
+
+	public void setTime(Double time) {
+		this.time = time;
+	}
+	
 	
 	
 	
