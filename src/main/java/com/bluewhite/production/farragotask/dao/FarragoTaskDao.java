baa001acae52fabaca23c655cbcf29556d9dba04
Zjiezi <597929460@qq.com>
2018-05-25 16:48:00 +0800

    *

---
+++ src/main/java/com/bluewhite/production/farragotask/dao/FarragoTaskDao.java
@@ -0,0 +1,8 @@
+package com.bluewhite.production.farragotask.dao;
+
+import com.bluewhite.base.BaseRepository;
+import com.bluewhite.production.farragotask.entity.FarragoTask;
+
+public interface FarragoTaskDao extends BaseRepository<FarragoTask, Long>{
+
+}
