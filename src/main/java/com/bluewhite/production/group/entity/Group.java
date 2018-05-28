20e5fd6ae4bd136539024bf911f48b221e42b948
Zjiezi <597929460@qq.com>
2018-05-28 09:51:45 +0800

    *

--- src/main/java/com/bluewhite/production/group/entity/Group.java
+++ src/main/java/com/bluewhite/production/group/entity/Group.java
@@ -24,12 +24,6 @@ public class Group  extends BaseEntity<Long>{
 	@Column(name = "name")
 	private String name;
 	
-//	/**
-//	 * 到岗小时预计收入
-//	 */
-//	@Column(name = "price")
-//	private Double price;
-	
 	/**
 	 * 分组所属部门类型 (1=一楼质检，2=一楼包装，3=二楼针工)
 	 */
@@ -68,14 +62,6 @@ public class Group  extends BaseEntity<Long>{
 		this.name = name;
 	}
 
-//	public Double getPrice() {
-//		return price;
-//	}
-//
-//	public void setPrice(Double price) {
-//		this.price = price;
-//	}
-	
 	
 	
 	
