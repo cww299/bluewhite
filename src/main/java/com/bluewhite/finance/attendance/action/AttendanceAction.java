a8a73edb4834e845411f53a785b2951464c49e6a
Zjiezi <597929460@qq.com>
2018-05-29 14:15:33 +0800

    *

--- src/main/java/com/bluewhite/finance/attendance/action/AttendanceAction.java
+++ src/main/java/com/bluewhite/finance/attendance/action/AttendanceAction.java
@@ -81,14 +81,15 @@ public class AttendanceAction {
 						}else{
 							attendance.setAllotTime(attendancePay.getAllotTime());
 						}
-						attendance.setOrderTimeBegin(DatesUtil.getFirstDayOfMonth(attendance.getAllotTime()));
-						attendance.setOrderTimeEnd(DatesUtil.getLastDayOfMonth(attendance.getAllotTime()));
+						//获取今天的开始和结束时间
+						attendance.setOrderTimeBegin(DatesUtil.getfristDayOftime(attendance.getAllotTime()));
+						attendance.setOrderTimeEnd(DatesUtil.getLastDayOftime(attendance.getAllotTime()));
+						attendance.setType(attendancePay.getType());
 						if(attendancePayService.findPages(attendance, page).getRows().size()>0){
 							cr.setMessage(user.getUserName()+"该月已存在考情记录，无需再次添加，请重新选择");
 							cr.setCode(ErrorCode.ILLEGAL_ARGUMENT.getCode());
 							return cr;
 						}else{
-							attendance.setType(attendancePay.getType());
 							attendance.setWorkTime(attendancePay.getWorkTimes()[i]);
 							attendance.setWorkPrice(user.getPrice());
 							attendance.setUserName(user.getUserName());
