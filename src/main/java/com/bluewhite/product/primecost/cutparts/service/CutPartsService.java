c4d47b1ca74093cc253c448cfe3eb98ecf9a0d54
Zjiezi <597929460@qq.com>
2018-07-04 10:53:18 +0800

    裁片

---
+++ src/main/java/com/bluewhite/product/primecost/cutparts/service/CutPartsService.java
@@ -0,0 +1,11 @@
+package com.bluewhite.product.primecost.cutparts.service;
+
+import org.springframework.stereotype.Service;
+
+import com.bluewhite.base.BaseCRUDService;
+import com.bluewhite.product.primecost.cutparts.entity.CutParts;
+
+@Service
+public interface CutPartsService extends BaseCRUDService<CutParts,Long>{
+
+}
