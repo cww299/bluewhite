2deb2dcd935bb457e8486044359bc9637c07020a
Zjiezi <597929460@qq.com>
2018-07-20 15:05:37 +0800

    *

--- src/main/java/com/bluewhite/product/primecost/cutparts/service/CutPartsService.java
+++ src/main/java/com/bluewhite/product/primecost/cutparts/service/CutPartsService.java
@@ -8,6 +8,6 @@ import com.bluewhite.product.primecost.cutparts.entity.CutParts;
 @Service
 public interface CutPartsService extends BaseCRUDService<CutParts,Long>{
 
-	public CutParts saveCutParts(CutParts cutParts);
+	public CutParts saveCutParts(CutParts cutParts) throws Exception;
 
 }
