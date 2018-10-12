e202aef206dcbff403b9dbb37675b700a3bfbe21
Zjiezi <597929460@qq.com>
2018-10-12 11:07:19 +0800

    *

--- src/main/java/com/bluewhite/reportexport/entity/UserPoi.java
+++ src/main/java/com/bluewhite/reportexport/entity/UserPoi.java
@@ -6,88 +6,133 @@ public class UserPoi {
 	
 	
 	/**
-	 * weizhi
 	 */
 	@Poi(name = "", column = "A")
-	private String number;
+	private String gender;
 	
 	/**
-	 * 姓名
 	 */
 	@Poi(name = "", column = "B")
-	private String userName;
+	private String id_card;
 	
 	/**
-	 * 联系方式
 	 */
 	@Poi(name = "", column = "C")
+	private String login_name;
+	
+	/**
+	 */
+	@Poi(name = "", column = "D")
 	private String phone;
 	
 	/**
-	 * 身份证号
-	 */
-	@Poi(name = "", column = "D")
-	private String idCard;
-	
-	/**
-	 * 入职时间
 	 */
 	@Poi(name = "", column = "E")
-	private String entry;
+	private String bank_card1;
 	
 	/**
-	 *离职时间
 	 */
 	@Poi(name = "", column = "F")
-    private String quitDate;
+    private String contract_date;
 	
 	
 	/**
-	 * 户籍地址
 	 */
 	@Poi(name = "", column = "G")
-    private String permanentAddress;
+    private String entry;
 	
 	/**
-	 * 现居住地址
 	 */
 	@Poi(name = "", column = "H")
-    private String livingAddress;
+    private String estimate;
 
+	/**
+	 */
+	@Poi(name = "", column = "I")
+    private String information;
 	
+	/**
+	 */
+	@Poi(name = "", column = "J")
+    private String living_address;
+	
+	/**
+	 */
+	@Poi(name = "", column = "K")
+    private String permanent_address;
+	
+	/**
+	 */
+	@Poi(name = "", column = "M")
+    private Integer quit;
+	
+	/**
+	 */
+	@Poi(name = "", column = "L")
+    private String quit_date;
+	
+	/**
+	 */
+	@Poi(name = "", column = "N")
+    private String foreigns;
+	
+	/**
+	 */
+	@Poi(name = "", column = "O")
+    private String telephone;
+	
+	/**
+	 */
+	@Poi(name = "", column = "P")
+    private String company;
+	
+	/**
+	 */
+	@Poi(name = "", column = "Q")
+    private String nexus;
+	
+	/**
+	 */
+	@Poi(name = "", column = "R")
+    private String safe;
+	
+	/**
+	 */
+	@Poi(name = "", column = "S")
+    private String commitment;
+	
+	/**
+	 */
+	@Poi(name = "", column = "T")
+    private String contacts;
+	
+	/**
+	 */
+	@Poi(name = "", column = "U")
+    private String user_contract_id;
 
-	public String getNumber() {
-		return number;
+	public String getGender() {
+		return gender;
 	}
 
-	public void setNumber(String number) {
-		this.number = number;
+	public void setGender(String gender) {
+		this.gender = gender;
 	}
 
-	public String getIdCard() {
-		return idCard;
+	public String getId_card() {
+		return id_card;
 	}
 
-	public void setIdCard(String idCard) {
-		this.idCard = idCard;
+	public void setId_card(String id_card) {
+		this.id_card = id_card;
 	}
 
-	public String getEntry() {
-		return entry;
+	public String getLogin_name() {
+		return login_name;
 	}
 
-	public void setEntry(String entry) {
-		this.entry = entry;
-	}
-
-
-
-	public String getQuitDate() {
-		return quitDate;
-	}
-
-	public void setQuitDate(String quitDate) {
-		this.quitDate = quitDate;
+	public void setLogin_name(String login_name) {
+		this.login_name = login_name;
 	}
 
 	public String getPhone() {
@@ -98,58 +143,145 @@ public class UserPoi {
 		this.phone = phone;
 	}
 
-//	public String getIdCard() {
-//		return idCard;
-//	}
-//
-//	public void setIdCard(String idCard) {
-//		this.idCard = idCard;
-//	}
-
-	public String getPermanentAddress() {
-		return permanentAddress;
+	public String getBank_card1() {
+		return bank_card1;
 	}
 
-	public void setPermanentAddress(String permanentAddress) {
-		this.permanentAddress = permanentAddress;
+	public void setBank_card1(String bank_card1) {
+		this.bank_card1 = bank_card1;
 	}
 
-	public String getLivingAddress() {
-		return livingAddress;
+	public String getContract_date() {
+		return contract_date;
 	}
 
-	public void setLivingAddress(String livingAddress) {
-		this.livingAddress = livingAddress;
+	public void setContract_date(String contract_date) {
+		this.contract_date = contract_date;
 	}
 
-	public String getUserName() {
-		return userName;
+	public String getEntry() {
+		return entry;
 	}
 
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public void setEntry(String entry) {
+		this.entry = entry;
 	}
-//
-//
-//
-//	public String getEntry() {
-//		return entry;
-//	}
-//
-//	public void setEntry(String entry) {
-//		this.entry = entry;
-//	}
-//
-//	public String getBirthday() {
-//		return birthday;
-//	}
-//
-//	public void setBirthday(String birthday) {
-//		this.birthday = birthday;
-//	}
-//
+
+	public String getEstimate() {
+		return estimate;
+	}
+
+	public void setEstimate(String estimate) {
+		this.estimate = estimate;
+	}
+
+	public String getInformation() {
+		return information;
+	}
+
+	public void setInformation(String information) {
+		this.information = information;
+	}
+
+	public String getLiving_address() {
+		return living_address;
+	}
+
+	public void setLiving_address(String living_address) {
+		this.living_address = living_address;
+	}
+
+	public String getPermanent_address() {
+		return permanent_address;
+	}
+
+	public void setPermanent_address(String permanent_address) {
+		this.permanent_address = permanent_address;
+	}
 
 	
+
+	public Integer getQuit() {
+		return quit;
+	}
+
+	public void setQuit(Integer quit) {
+		this.quit = quit;
+	}
+
+	public String getQuit_date() {
+		return quit_date;
+	}
+
+	public void setQuit_date(String quit_date) {
+		this.quit_date = quit_date;
+	}
+
+	public String getForeigns() {
+		return foreigns;
+	}
+
+	public void setForeigns(String foreigns) {
+		this.foreigns = foreigns;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getNexus() {
+		return nexus;
+	}
+
+	public void setNexus(String nexus) {
+		this.nexus = nexus;
+	}
+
+	public String getSafe() {
+		return safe;
+	}
+
+	public void setSafe(String safe) {
+		this.safe = safe;
+	}
+
+	public String getCommitment() {
+		return commitment;
+	}
+
+	public void setCommitment(String commitment) {
+		this.commitment = commitment;
+	}
+
+	public String getContacts() {
+		return contacts;
+	}
+
+	public void setContacts(String contacts) {
+		this.contacts = contacts;
+	}
+
+	public String getUser_contract_id() {
+		return user_contract_id;
+	}
+
+	public void setUser_contract_id(String user_contract_id) {
+		this.user_contract_id = user_contract_id;
+	}
+	
+
 
 	
 
