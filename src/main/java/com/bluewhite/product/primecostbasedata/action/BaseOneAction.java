e1088d94c3d8f2792a364cb0e0713d03a8820250
Zjiezi <597929460@qq.com>
2018-10-26 17:26:23 +0800

    *

--- src/main/java/com/bluewhite/product/primecostbasedata/action/BaseOneAction.java
+++ src/main/java/com/bluewhite/product/primecostbasedata/action/BaseOneAction.java
@@ -219,8 +219,6 @@ public class BaseOneAction {
 		if(primeCoefficient.getId()!=null){
 			PrimeCoefficient oldPrimeCoefficient = primeCoefficientDao.findOne(primeCoefficient.getId());
 			BeanCopyUtils.copyNullProperties(oldPrimeCoefficient,primeCoefficient);
-			//每CM 用时/秒
-			primeCoefficient.setTime(1/primeCoefficient.getExtent());
 			//被/数
 			primeCoefficient.setQuilt(primeCoefficient.getQuilt());
 			//每秒设备折旧费用
