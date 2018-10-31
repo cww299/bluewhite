beb4f8b855c4439930ba083d575917cbe3fb8fc7
Zjiezi <597929460@qq.com>
2018-10-31 14:10:47 +0800

    *

--- src/main/java/com/bluewhite/product/primecost/embroidery/service/EmbroideryServiceImpl.java
+++ src/main/java/com/bluewhite/product/primecost/embroidery/service/EmbroideryServiceImpl.java
@@ -48,11 +48,11 @@ public class EmbroideryServiceImpl extends BaseServiceImpl<Embroidery, Long> imp
 
 		PrimeCoefficient primeCoefficient = primeCoefficientDao.findByType("embroidery");
 		// 单片机走时间
-		embroidery.setSinglechipApplique(NumUtils.round(
-				(primeCoefficient.getEmbroideryTwo() / 1000) * embroidery.getNeedleNumber() / embroidery.getFew(), 1));
+		embroidery.setSinglechipApplique(NumUtils.division(NumUtils.round(
+				(primeCoefficient.getEmbroideryTwo() / 1000) * embroidery.getNeedleNumber() / embroidery.getFew(), 1)));
 		// 铺布或裁片秀贴布和上绷子时间
-		embroidery.setTime(embroidery.getCloth() == null ? primeCoefficient.getEmbroideryFour()
-				: primeCoefficient.getEmbroideryThree() / embroidery.getEmbroiderySlice());
+		embroidery.setTime(NumUtils.division(embroidery.getCloth() == null ? primeCoefficient.getEmbroideryFour()
+				: primeCoefficient.getEmbroideryThree() / embroidery.getEmbroiderySlice()));
 		// 贴片时间,通过基础数据三将数值返回
 		double pasterTime = 0;
 		if (!StringUtils.isEmpty(embroidery.getAppliqueTime())) {
@@ -102,20 +102,20 @@ public class EmbroideryServiceImpl extends BaseServiceImpl<Embroidery, Long> imp
 						+ embroidery.getEquipmentPrice() + embroidery.getAdministrativeAtaff(), 3));
 		// 目前市场价格
 		embroidery.setReckoningSewingPrice(
-				((NumUtils.round((double) embroidery.getNeedleNumber() / 1000, 0) * 1000) < embroidery.getNeedleNumber()
+				(NumUtils.division((NumUtils.round((double) embroidery.getNeedleNumber() / 1000, 0) * 1000) < embroidery.getNeedleNumber()
 						? (NumUtils.round((double) embroidery.getNeedleNumber() / 1000, 0) * 1000 + 1000)
 						: (NumUtils.round((double) embroidery.getNeedleNumber() / 1000, 0) * 1000)) / 1000
 						* primeCoefficient.getEmbroideryTwelve()
 						+ (embroidery.getApplique() * primeCoefficient.getEmbroideryThirteen())
-						+ (embroidery.getEmbroideryColorNumber() - 1) * primeCoefficient.getEmbroideryFourteen());
+						+ (embroidery.getEmbroideryColorNumber() - 1) * primeCoefficient.getEmbroideryFourteen()));
 		// 入成本价格
 		embroidery.setAllCostPrice(embroidery.getNumber() * NumUtils.setzro(embroidery.getCostPrice()));
 		// 各单道比全套工价
 		List<Embroidery> embroideryList = dao.findByProductId(embroidery.getProductId());
 		embroideryList.add(embroidery);
-		double scaleMaterial = embroidery.getAllCostPrice()
+		double scaleMaterial = NumUtils.division(embroidery.getAllCostPrice()
 				/ (embroideryList.stream().filter(Machinist -> Machinist.getAllCostPrice() != null)
-						.mapToDouble(Embroidery::getAllCostPrice).sum());
+						.mapToDouble(Embroidery::getAllCostPrice).sum()));
 		embroidery.setScaleMaterial(scaleMaterial);
 		// 物料和上道压（裁剪）价(裁片绣上道压价 = 裁剪页面的不含绣花环节的为机工压价 ， 整布绣上道压价 = cc裁片页面 当批各单片价格)
 		List<Tailor> tailorList = tailorDao.findByProductId(embroidery.getProductId());
