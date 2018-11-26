6e3963c803fa878b41c6e0fd5bd87f74759ebe8d
Zjiezi <597929460@qq.com>
2018-11-26 10:17:17 +0800

    *

---
+++ src/main/java/com/bluewhite/finance/ledger/dao/BillDao.java
@@ -0,0 +1,8 @@
+package com.bluewhite.finance.ledger.dao;
+
+import com.bluewhite.base.BaseRepository;
+import com.bluewhite.finance.ledger.entity.Bill;
+
+public interface BillDao extends BaseRepository<Bill, Long>{
+
+}
