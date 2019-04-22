1aa3c5ee43c5aaa38fad46a62de64b4abfd09752
Zjiezi <597929460@qq.com>
2019-04-22 15:11:40 +0800

    *

--- src/main/java/com/bluewhite/onlineretailers/inventory/action/InventoryAction.java
+++ src/main/java/com/bluewhite/onlineretailers/inventory/action/InventoryAction.java
@@ -1,14 +1,36 @@
 package com.bluewhite.onlineretailers.inventory.action;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.ocean.rawsdk.ApiExecutor;
+import com.bluewhite.common.entity.CommonResponse;
+import com.bluewhite.finance.attendance.entity.AttendancePay;
+@Controller
 public class InventoryAction {
 	
 	
-	public static void main(String[] args) {
+	/** 
+	 * 
+	 * 
+	 */
+	@RequestMapping(value = "/inventory/test", method = RequestMethod.GET)
+	@ResponseBody
+	public CommonResponse test(HttpServletRequest request) {
+		CommonResponse cr = new CommonResponse();
 		
-		//设置appkey和密钥(seckey)
-
+		return cr;
 	}
 	
-	
+
+	public static void main(String[] args) {
+		//设置appkey和密钥(seckey)
+		ApiExecutor apiExecutor = new ApiExecutor("4879538","oBhKXN1W38"); 
+	}
 
 }
--- src/main/java/com/bluewhite/onlineretailers/inventory/entity/Commodity.java
+++ src/main/java/com/bluewhite/onlineretailers/inventory/entity/Commodity.java
@@ -1,5 +1,23 @@
 package com.bluewhite.onlineretailers.inventory.entity;
 
 public class Commodity {
+	
+	/**
+	 * 商品编号
+	 * 
+	 */
+	private String number;
+	
+	/**
+	 * 商品重量
+	 * 
+	 */
+//	private String number;
+	
+	/**
+	 * 商品备注
+	 * 
+	 */
+//	private String number;
 
 }
