2a7d6afc2c37872f4153986d816743e428dcb017
Zjiezi <597929460@qq.com>
2019-05-23 09:53:10 +0800

    *

--- src/main/java/com/bluewhite/finance/consumption/action/ConsumptionAction.java
+++ src/main/java/com/bluewhite/finance/consumption/action/ConsumptionAction.java
@@ -51,7 +51,7 @@ public class ConsumptionAction {
 				.get()
 				.addRetainTerm(Consumption.class, "id", "user","content","userId"
 				,"budget","money","expenseDate","paymentMoney","paymentDate","withholdReason","remark"
-				,"withholdMoney","settleAccountsMode","remark","flag","taxPoint","custom","user","contact","logisticsDate","contactName")
+				,"withholdMoney","settleAccountsMode","remark","flag","taxPoint","custom","user","contact","logisticsDate","contactName","bacthNumber")
 				.addRetainTerm(User.class, "userName")
 				.addRetainTerm(Contact.class, "conPartyNames");
 	}
