5205969b4008058943abbb85ff1210f90a56e82f
Zjiezi <597929460@qq.com>
2019-07-23 08:31:59 +0800

    贴包

---
+++ src/main/java/com/bluewhite/ledger/action/LedgerAction.java
@@ -0,0 +1,159 @@
+package com.bluewhite.ledger.action;
+
+import java.text.SimpleDateFormat;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.support.ByteArrayMultipartFileEditor;
+
+import com.bluewhite.basedata.entity.BaseData;
+import com.bluewhite.common.ClearCascadeJSON;
+import com.bluewhite.common.DateTimePattern;
+import com.bluewhite.common.entity.CommonResponse;
+import com.bluewhite.common.entity.PageParameter;
+import com.bluewhite.ledger.entity.Packing;
+import com.bluewhite.ledger.entity.PackingChild;
+import com.bluewhite.ledger.entity.SendGoods;
+import com.bluewhite.ledger.service.PackingService;
+import com.bluewhite.ledger.service.SendGoodsService;
+import com.bluewhite.product.product.entity.Product;
+
+/**
+ * 销售
+ * 
+ * @author zhangliang
+ *
+ */
+@Controller
+public class LedgerAction {
+
+	@Autowired
+	private PackingService packingService;
+	@Autowired
+	private SendGoodsService sendGoodsService;
+
+
+	private ClearCascadeJSON clearCascadeJSON;
+	{
+		clearCascadeJSON = ClearCascadeJSON.get()
+				.addRetainTerm(Packing.class, "number", "customr","packingMaterials","packingChilds","packingDate")
+				.addRetainTerm(PackingChild.class, "bacthNumber", "product","count")
+				.addRetainTerm(Product.class, "name")
+				.addRetainTerm(BaseData.class, "name");
+	}
+	
+	private ClearCascadeJSON clearCascadeJSON1;
+	{
+		clearCascadeJSON = ClearCascadeJSON.get()
+				.addRetainTerm(Packing.class, "number", "customr","packingMaterials","packingChilds","packingDate")
+				.addRetainTerm(PackingChild.class, "bacthNumber", "product","count")
+				.addRetainTerm(Product.class, "name")
+				.addRetainTerm(BaseData.class, "name");
+	}
+	
+	/**
+	 * 查看贴包单
+	 * 
+	 * @param request 请求
+	 * @return cr
+	 */
+	@RequestMapping(value = "/ledger/getPacking", method = RequestMethod.GET)
+	@ResponseBody
+	public CommonResponse getPacking(PageParameter page,Packing packing) {
+		CommonResponse cr = new CommonResponse();
+		cr.setData(clearCascadeJSON.format(packingService.findPages(packing, page)).toJSON());
+		cr.setMessage("查看成功");
+		return cr;
+	}
+	
+	/**
+	 * 新增贴包单
+	 * 
+	 * @param request 请求
+	 * @return cr
+	 */
+	@RequestMapping(value = "/ledger/addPacking", method = RequestMethod.POST)
+	@ResponseBody
+	public CommonResponse addPacking(Packing packing) { 
+		CommonResponse cr = new CommonResponse();
+		packingService.addPacking(packing);
+ 		cr.setMessage("新增成功");
+		return cr;
+	}
+	
+	
+	/**
+	 * 获取编号
+	 * 
+	 */
+	@RequestMapping(value = "/ledger/getPackingNumber", method = RequestMethod.POST)
+	@ResponseBody
+	public CommonResponse getPackingNumber() {
+		CommonResponse cr = new CommonResponse();
+		String packingNumber = packingService.getPackingNumber();
+		cr.setData(packingNumber);
+		cr.setMessage("新增成功");
+		return cr;
+	}
+	
+	
+	/**
+	 * 查看发货单
+	 * 
+	 * @param request 请求
+	 * @return cr
+	 */
+	@RequestMapping(value = "/ledger/getSendGoods", method = RequestMethod.GET)
+	@ResponseBody
+	public CommonResponse getSendGoods(PageParameter page,SendGoods sendGoods) {
+		CommonResponse cr = new CommonResponse();
+		cr.setData(clearCascadeJSON1.format(sendGoodsService.findPages(sendGoods, page)).toJSON());
+		cr.setMessage("查看成功");
+		return cr;
+	}
+	
+	/**
+	 * 通过条件查找发货单
+	 * 
+	 * @param request 请求
+	 * @return cr
+	 */
+	@RequestMapping(value = "/ledger/getSearchSendGoods", method = RequestMethod.GET)
+	@ResponseBody
+	public CommonResponse getSearchSendGoods(SendGoods sendGoods) {
+		CommonResponse cr = new CommonResponse();
+		cr.setData(clearCascadeJSON1.format(sendGoodsService.findLists(sendGoods )).toJSON());
+		cr.setMessage("查看成功");
+		return cr;
+	}
+	
+	/**
+	 * 新增发货单
+	 * 
+	 * @param request 请求
+	 * @return cr
+	 */
+	@RequestMapping(value = "/ledger/addSendGoods", method = RequestMethod.POST)
+	@ResponseBody
+	public CommonResponse addSendGoods(SendGoods sendGoods) { 
+		CommonResponse cr = new CommonResponse();
+		sendGoodsService.addSendGoods(sendGoods);
+ 		cr.setMessage("新增成功");
+		return cr;
+	}
+	
+	
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+		SimpleDateFormat dateTimeFormat = new SimpleDateFormat(DateTimePattern.DATEHMS.getPattern());
+		binder.registerCustomEditor(java.util.Date.class, null, new CustomDateEditor(dateTimeFormat, true));
+		binder.registerCustomEditor(byte[].class, new ByteArrayMultipartFileEditor());
+	}
+	
+}
---
+++ src/main/java/com/bluewhite/ledger/service/PackingServiceImpl.java
@@ -0,0 +1,98 @@
+package com.bluewhite.ledger.service;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.criteria.Predicate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.bluewhite.base.BaseServiceImpl;
+import com.bluewhite.common.ServiceException;
+import com.bluewhite.common.entity.PageParameter;
+import com.bluewhite.common.entity.PageResult;
+import com.bluewhite.ledger.dao.PackingDao;
+import com.bluewhite.ledger.entity.Packing;
+import com.bluewhite.ledger.entity.PackingChild;
+import com.bluewhite.onlineretailers.inventory.entity.OnlineOrderChild;
+
+@Service
+public class PackingServiceImpl extends BaseServiceImpl<Packing, Long> implements PackingService {
+
+	@Autowired
+	private PackingDao dao;
+
+	@Override
+	public PageResult<Packing> findPages(Packing param, PageParameter page) {
+		Page<Packing> pages = dao.findAll((root, query, cb) -> {
+			List<Predicate> predicate = new ArrayList<>();
+			// 按id过滤
+			if (param.getId() != null) {
+				predicate.add(cb.equal(root.get("id").as(Long.class), param.getId()));
+			}
+			// 按客户id过滤
+			if (param.getCustomrId() != null) {
+				predicate.add(cb.equal(root.get("customrId").as(Long.class), param.getCustomrId()));
+			}
+			// 按产品id过滤
+			if (param.getProductId() != null) {
+				predicate
+						.add(cb.equal(root.get("packingChilds").get("productId").as(Long.class), param.getProductId()));
+			}
+			// 按批次查找
+			if (!StringUtils.isEmpty(param.getBacthNumber())) {
+				predicate.add(cb.like(root.get("packingChilds").get("bacthNumber").as(String.class),
+						"%" + param.getBacthNumber() + "%"));
+			}
+			// 按发货贴包日期
+			if (!StringUtils.isEmpty(param.getOrderTimeBegin()) && !StringUtils.isEmpty(param.getOrderTimeEnd())) {
+				predicate.add(cb.between(root.get("packingDate").as(Date.class), param.getOrderTimeBegin(),
+						param.getOrderTimeEnd()));
+			}
+			Predicate[] pre = new Predicate[predicate.size()];
+			query.where(predicate.toArray(pre));
+			return null;
+		}, page);
+		PageResult<Packing> result = new PageResult<>(pages, page);
+		return result;
+	}
+
+	@Override
+	public String getPackingNumber() {
+		Calendar now = Calendar.getInstance();
+		List<Packing> PackingList = dao.findByPackingDate(now.getTime());
+		String year = String.valueOf(now.get(Calendar.YEAR));
+		String month =  String.valueOf(now.get(Calendar.MONTH) + 1);
+		String day =  String.valueOf(now.get(Calendar.DAY_OF_MONTH));
+		String yearString = year.substring(year.length() -2,year.length());   //截取最后两位
+		String packingNumber = yearString+"N"+month+"Y"+day+"R" +(PackingList.size()+1)+"D";
+		return packingNumber;
+	}
+
+	@Override
+	public Packing addPacking(Packing packing) {
+		packing.setPackingDate(packing.getPackingDate()!=null ? packing.getPackingDate() : new Date());
+		// 新增子单
+		if (!StringUtils.isEmpty(packing.getChildPacking())) {
+			JSONArray jsonArray = JSON.parseArray(packing.getChildPacking());
+			for (int i = 0; i < jsonArray.size(); i++) {
+				PackingChild packingChild = new PackingChild();
+				JSONObject jsonObject = jsonArray.getJSONObject(i);
+				packingChild.setBacthNumber(jsonObject.getString("bacthNumber"));
+				packingChild.setProductId(jsonObject.getLong("productId"));
+				packingChild.setCount(jsonObject.getInteger("count"));
+				packing.getPackingChilds().add(packingChild);
+			}
+		}
+		dao.save(packing);
+ 		return packing;
+	}
+}
