86ab136081790ef9109475b2cfee9f1a2a4ac012
Zjiezi <597929460@qq.com>
2019-08-02 11:37:48 +0800

    *

--- src/main/java/com/bluewhite/ledger/service/MixedService.java
+++ src/main/java/com/bluewhite/ledger/service/MixedService.java
@@ -8,11 +8,28 @@ import com.bluewhite.common.entity.PageResult;
 import com.bluewhite.ledger.entity.Mixed;
 
 @Service
-public interface MixedService extends BaseCRUDService<Mixed,Long>{
-	public PageResult<Mixed>  findPages(Mixed mixed, PageParameter page);
-	
-	
+public interface MixedService extends BaseCRUDService<Mixed, Long> {
+	/**
+	 * 分頁查看杂项支出
+	 * 
+	 * @param mixed
+	 * @param page
+	 * @return
+	 */
+	public PageResult<Mixed> findPages(Mixed mixed, PageParameter page);
+
+	/**
+	 * 新增杂支
+	 * 
+	 * @param mixed
+	 */
 	public void addMixed(Mixed mixed);
+
+	/**
+	 * 删除杂支
+	 * 
+	 * @param ids
+	 */
 	public void deleteMixed(String ids);
-	
+
 }
