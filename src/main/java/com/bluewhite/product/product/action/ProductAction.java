32f5e1b9f68eb6982776d81ce24f4a40c093abff
Zjiezi <597929460@qq.com>
2019-08-30 11:41:58 +0800

    *

--- src/main/java/com/bluewhite/product/product/action/ProductAction.java
+++ src/main/java/com/bluewhite/product/product/action/ProductAction.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.bluewhite.common.BeanCopyUtils;
 import com.bluewhite.common.ClearCascadeJSON;
 import com.bluewhite.common.Constants;
-import com.bluewhite.common.Log;
 import com.bluewhite.common.SessionManager;
 import com.bluewhite.common.annotation.SysLogAspectAnnotation;
 import com.bluewhite.common.entity.CommonResponse;
