f6260c051380a635baaa0ca36edb0494e5db2c96
Zjiezi <597929460@qq.com>
2019-10-04 17:26:15 +0800

    *

--- src/main/java/com/bluewhite/system/user/dao/TemporaryUserDao.java
+++ src/main/java/com/bluewhite/system/user/dao/TemporaryUserDao.java
@@ -1,8 +1,18 @@
 package com.bluewhite.system.user.dao;
 
+import java.util.List;
+
 import com.bluewhite.base.BaseRepository;
 import com.bluewhite.system.user.entity.TemporaryUser;
+import com.bluewhite.system.user.entity.User;
 
 public interface TemporaryUserDao extends BaseRepository<TemporaryUser, Long> {
+	
+	/**
+	 * 通过多个id查找用户
+	 * @param ids 用户ids
+	 * @return list
+	 */
+	public List<TemporaryUser> findByIdIn(List<Long> ids);
 
 }
