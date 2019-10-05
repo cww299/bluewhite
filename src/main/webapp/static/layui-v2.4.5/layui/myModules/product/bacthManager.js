39dece92c665292cce32ad9d4d613c42ca783009
cww <chenxxvv@foxmail.com>
2019-10-05 17:55:30 +0800

    滚动条样式修改、分配增加遮罩

--- src/main/webapp/static/layui-v2.4.5/layui/myModules/product/bacthManager.js
+++ src/main/webapp/static/layui-v2.4.5/layui/myModules/product/bacthManager.js
@@ -336,6 +336,7 @@ layui.config({
 						form.render();
 					},
 					yes:function(){
+						var load = layer.load(1);
 						var userIds = [],procedureIds = [],temporaryUserIds = [];
 						var userTreeId = menuTree.getVal('userTree'),procedureTreeId = menuTree.getVal('procedureTree');
 						for(var i in userTreeId){	
@@ -372,6 +373,7 @@ layui.config({
 								table.reload('tableData');
 							}
 						})
+						layer.close(load);
 					}
 				})
 			}
