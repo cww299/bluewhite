0de55fb758dac5655aac6e73b50e5ecfd97e197a
cww <chenxxvv@foxmail.com>
2019-10-15 11:20:17 +0800

    员工分组增加一键休息、工作

--- src/main/webapp/static/layui-v2.4.5/layui/myModules/product/usergroup.js
+++ src/main/webapp/static/layui-v2.4.5/layui/myModules/product/usergroup.js
@@ -247,7 +247,9 @@ layui.config({
 								elem:'#lookoverTable',
 								url: opt.ctx+'/production/allGroup?id='+obj.data.id,
 								where:{ temporarilyDate: day,  },
-								toolbar:'<div><span class="layui-btn layui-btn-danger layui-btn-sm" lay-event="deletes">批量删除</span></div>',
+								toolbar:'<div><span class="layui-btn layui-btn-danger layui-btn-sm" lay-event="deletes">批量删除</span>'+
+								        '<span class="layui-btn layui-btn-sm" lay-event="oneKeyRest">一键休息</span>'+
+								        '<span class="layui-btn layui-btn-sm" lay-event="oneKeyWork">一键工作</span></div>',
 								cols:[ [
 									{ type:'checkbox', },
 									{ field:'name', title:'人名' },
@@ -319,7 +321,7 @@ layui.config({
 														data:{
 															id: trData.userId,
 															status: data.elem.checked?1:0,
-																	time: $('#outTime').val(),
+															time: $('#outTime').val(),
 														},
 														success:function(){
 															layer.close(confirm);
@@ -346,9 +348,14 @@ layui.config({
 							}
 							table.on('toolbar(lookoverTable)',function(obj){
 								var checked = layui.table.checkStatus('lookoverTable').data;
-								if(obj.event=='deletes'){
-									if(checked.length==0)
-										return myutil.emsg('请选择信息删除');
+								if(checked.length==0)
+									return myutil.emsg('请选择相关信息');
+								switch(obj.evnet){
+								case 'deletest': deletest(); break;
+								case 'oneKeyWork': oneKeyWork(); break;
+								case 'oneKeyRest': oneKeyRest(); break;
+								}
+								function deletest(){
 									var temporarilyIds = [];
 									for(var i in checked)
 										if(checked[i].secondment=='1')
@@ -359,6 +366,62 @@ layui.config({
 										url: '/production/deleteTemporarily',
 										ids: temporarilyIds.join(','),
 										success:function(){
+											table.reload('lookoverTable');
+										}
+									})
+								}
+								function oneKeyWork(){
+									for(var i in checked){
+										myutil.saveAjax({
+											url: '/production/updateManualTime',
+											type: 'get',
+											data:{
+												id: checked[i].userId,
+												status: 1,
+											},
+										})
+									}
+								}
+								function oneKeyRest(){
+									var html = [
+									            '<div style="padding:15px;">',
+								                	'<form class="layui-form layui-form-pane" action="">',
+								                	  '<div class="layui-form-item" pane>',
+								                	    '<label class="layui-form-label">签出时间</label>',
+								                	    '<div class="layui-input-block">',
+								                	      '<input type="text" id="outTime" class="layui-input">',
+								                	    '</div>',
+								                	  '</div>',
+								                	'</form>',
+									            '</div>',
+									            ].join(' ');
+									var confirm = layer.open({
+										type:1,
+										content: html,
+										offset:'100px',
+										area: ['45%','30%'],
+										btn:['确定','取消'],
+										success:function(){
+											laydate.render({
+												elem: '#outTime',
+												type: 'datetime',
+												value: new Date().format('yyyy-MM-dd hh:mm:ss'),
+											})
+										},
+										yes: function(){
+											for(var i in checked)
+												myutil.saveAjax({
+													url: '/production/updateManualTime',
+													type: 'get',
+													data:{
+														id: checked[i].userId,
+														status: 0,
+														time: $('#outTime').val(),
+													},
+												})
+											layer.close(confirm);
+										},
+										end:function(){
 											table.reload('lookoverTable');
 										}
 									})
