8c4188621984a5d57f2c4214fa2943628e6b86a6
cww <chenxxvv@foxmail.com>
2019-10-18 14:07:42 +0800

    修改成功后的回调

--- src/main/webapp/static/layui-v2.4.5/layui/myModules/mytable.js
+++ src/main/webapp/static/layui-v2.4.5/layui/myModules/mytable.js
@@ -11,7 +11,7 @@
  * 					cols:[[ { type:'price', }  ]]   修改时只能是数字，不能为空
  * 					cols:[[ { type:'count', }  ]]	修改时只能是正整数
  * 开启自动修改功能：autoUpdate:{  saveUrl:'新增的接口', deleUrl:'删除接口', updateUrl:'修改接口（如果不存在时，默认使用新增接口）',
- * 									field:{ 虚拟字段:'对应的上传值' },isReload：修改成功是否重载表格  },   如：customer_id 对应的上传值customerId
+ * 									field:{ 虚拟字段:'对应的上传值' },isReload：修改成功是否重载表格 ,success:成功后的回调 },   如：customer_id 对应的上传值customerId
  * 增加自动curd工具模板：curd: {
  * 							btn:[1,2,3,4],  需要显示的按钮，按顺序，默认全显
  *							addTemp:{ },  新增一行给定的默认值。不给的时候、默认为空值
@@ -232,6 +232,9 @@ layui.extend({
 									myutil.saveAjax({
 										url: opt.autoUpdate.updateUrl || opt.autoUpdate.saveUrl,
 										data: data,
+										success:function(){
+											opt.autoUpdate.success && opt.autoUpdate.success();
+										}
 									})
 								}
 								trData[f] = val;    	//修改缓存值
@@ -260,6 +263,7 @@ layui.extend({
 									success: function(){
 										if(opt.autoUpdate.isReload)
 											table.reload(tableId);
+										opt.autoUpdate.success && opt.autoUpdate.success();
 									}
 								})
 							}
@@ -297,6 +301,7 @@ layui.extend({
 							success: function(){
 								if(opt.autoUpdate.isReload)
 									table.reload(tableId);
+								opt.autoUpdate.success && opt.autoUpdate.success();
 							},
 							error: function(){
 								var index = $(obj.tr).data('index');
