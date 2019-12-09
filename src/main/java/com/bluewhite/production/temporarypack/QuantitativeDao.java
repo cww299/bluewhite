0110e8dd4ebcde3f29aa7608308ad7da664c8014
Zjiezi <597929460@qq.com>
2019-12-09 11:19:59 +0800

    *

--- src/main/java/com/bluewhite/production/temporarypack/QuantitativeDao.java
+++ src/main/java/com/bluewhite/production/temporarypack/QuantitativeDao.java
@@ -14,4 +14,10 @@ public interface QuantitativeDao  extends BaseRepository<Quantitative, Long>{
 	 */
 	@Query("SELECT distinct qc.id FROM Quantitative q,QuantitativeChild qc,UnderGoods u WHERE qc.underGoodsId = u.id AND q.flag = 1 and u.id = (?1)")
 	List<Long> findSendNumber(Long id);
+	
+	/**
+	 * 查询下货单已发货数量
+	 */
+	@Query("SELECT distinct qc.id FROM Quantitative q,QuantitativeChild qc,UnderGoods u WHERE qc.underGoodsId = u.id and u.id = (?1)")
+	List<Long> findStickNumber(Long id);
 }
