f83f7b559020020665113febb8d1de27badbc8d4
Zjiezi <597929460@qq.com>
2019-12-26 16:40:51 +0800

    *

--- src/main/java/com/bluewhite/production/temporarypack/QuantitativeDao.java
+++ src/main/java/com/bluewhite/production/temporarypack/QuantitativeDao.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import com.bluewhite.base.BaseRepository;
+import java.util.Date;
+import com.bluewhite.production.temporarypack.Quantitative;
 
 public interface QuantitativeDao  extends BaseRepository<Quantitative, Long>{
 
@@ -20,4 +22,12 @@ public interface QuantitativeDao  extends BaseRepository<Quantitative, Long>{
 	 */
 	@Query("SELECT distinct qc.id FROM Quantitative q,QuantitativeChild qc,UnderGoods u WHERE qc.underGoodsId = u.id and u.id = (?1)")
 	List<Long> findStickNumber(Long id);
+	
+	/**
+	 * 根据贴包日期
+	 * @param time
+	 * @return
+	 */
+	List<Quantitative> findByTimeBetween(Date startTime ,Date endTime);
 }
+
